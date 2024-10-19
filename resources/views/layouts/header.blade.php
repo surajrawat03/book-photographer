e33a3993fe0d3cc52dc05d41145a4ad67ae0cf50
mohan-rawat <surajrawat95699@gmail.com>
2024-10-19 11:25:19 +0530

    Added static public home page

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm sticky-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="{{ asset('images/logo.png') }}" alt="SnapCapture Logo" height="40">
+            <span class="h4 font-weight-bolder theme-color">SnapCapture</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto header-menu">
+                <li class="nav-item">
+                    <a class="header-menu-item nav-link selected" href="#">Book Photographer</a>
+                </li>
+                <li class="nav-item">
+                    <a class="header-menu-item nav-link" href="#">Quick Booking</a>
+                </li>
+                <li class="nav-item">
+                    <a class="header-menu-item nav-link" href="#">Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a class="header-menu-item nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto header-menu">
+                <li class="nav-item">
+                    <a class="header-menu-item nav-link" href="#"><i class="fas fa-sign-in-alt mr-1"></i>Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="header-menu-item nav-link" href="#">Register</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+  </nav>
